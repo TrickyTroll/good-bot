135cf9ace2d991f076e9ca927dfb7635bddb2436
Etienne Parent <eparent@MacBook-Pro.local>
2020-06-02 12:20:35 -0400

    Started editing paths for so_it_begins.py.

--- so_it_begins.py
+++ so_it_begins.py
@@ -1,12 +1,25 @@
 import sys
 from pathlib import Path
-sys.path.insert(1, '/home/app/app')
+# According to the Dockerfile, everything is going to run in the directory /home/all
+main_path = Path('/home/app')
+# Since we don't want to record this directory, the main script is going to run 
+# inside of a different one, called /tutorial.
+working_path = Path('/tutorial')
+# To access the user's video script, his or her $PWD will be mounted under /toto.
+mount_path = Path('/toto')
+import os
+# To import from functions while being inside of /tutorial, we need to insert 
+# a different path. functions.py is under /app.
+sys.path.insert(1, main_path / 'app')
 from functions import *
 
+# This script is still running under /tutorial, and is fed a path towards a markdown file.
+# This path is relative to /toto, since that's where the $PWD was binded.
 filename = Path(sys.argv[1])
-print(filename)
+script_path = mount_path / filename
 # Fetching the instructions inside script/script.md:
-instructions_list = instruction_finder(filename)
+instructions_list = instruction_finder(script_path)
+
 
 # Creating bash scripts for every item inside of the list:
 script_maker(instructions_list)
