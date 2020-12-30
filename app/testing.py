a00ad9c7b5c9e297c07f2880142ed100257bcbcd
TrickyTroll <tricky@beon.ca>
2020-12-30 13:22:32 -0500

    🎉 Begin: Good Bot app.

--- app/testing.py
+++
@@ -1,8 +0,0 @@
-from functions import *
-import time
-from pathlib import Path
-
-instructions_list = instruction_finder(Path('./'), Path('test.md'))
-
-script_maker(instructions_list, Path('./'), Path('./'))
-
