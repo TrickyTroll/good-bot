9603cb6deba74387a90cb88dc49f74f6872ed99e
TrickyTroll <tricky@beon.ca>
2021-01-04 12:00:55 -0500

    🐛 Fix: Changed for the right branch.

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 package main
 
-import "https://github.com/TrickyTroll/good-bot/cmd"
+import "https://github.com/TrickyTroll/good-bot/tree/latest/cmd"
 
 func main() {
 	cmd.Execute()
