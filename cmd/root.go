5b055aabf4c770890a3e69e215d9d0363ed8a5eb
TrickyTroll <tricky@beon.ca>
2021-01-04 15:52:24 -0500

    🚧 In progress: Still working on the cli

--- cmd/root.go
+++ cmd/root.go
@@ -25,7 +25,10 @@ import (
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	cfgFile     string
+	userLicense string
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -50,15 +53,10 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.good-bot.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVar(&cfgFile,
+		"config",
+		"",
+		"config file (default is $HOME/.good-bot.yaml)")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -74,9 +72,10 @@ func initConfig() {
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".good-bot" (without extension).
+		// Search config in home directory with name ".good-bot" (yaml extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".good-bot")
+		viper.SetConfigType("yaml")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
