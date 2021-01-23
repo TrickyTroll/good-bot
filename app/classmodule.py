ff6d1e9f2300b145788ee49ed668956eb57358de
TrickyTroll <tricky@beon.ca>
2021-01-23 16:08:13 -0500

    🎨 Improved: Commands class

--- app/classmodule.py
+++ app/classmodule.py
@@ -1,8 +1,10 @@
 import pexpect
-class Command:
-    def __init__(command: str, arguments: list):
-        self.command = command
-        self.arguments = arguments
+import sys
+class Commands:
+    def __init__(commands: list, expect: list):
+        self.commands = commands
+        self.expect = expect
+        self.initial = commands[0]
 
     def fake_start(self, text: str) -> None:
         """To print the first command before creating a child process.
@@ -23,10 +25,44 @@ class Command:
 
         return None
 
-    def 
+    def fake_typing(self, text: str) -> None:
+        """Fake typing of commands
+
+        Args:
+            text (str): The text to type
+
+        Returns:
+            None: None
+        """
+        letters = list(text)
+        letters.append("\n")
+        for letter in letters:
+            time.sleep(.12) #TODO: This should also be random.
+            child.send(things)
+        
+        return None
+    
+    def fake_typing_secret(self, secret: str) -> None:
+        """To fake type a password or other secret. This ensures that the
+        password won't be recorded.
+
+        Args:
+            secret (str): The secret that has to be typed
+
+        Returns:
+            None: None
+        """
+
+        child.logfile = None
+        child.logfile_read = sys.stdout
+        child.delaybeforesend = 1
+        child.sendline(password)
+        child.logfile = sys.stdout
+        child.logfile_read = None
+
+        return None
 
     def run(self):
-        pass
 
 class Read:
     def __init__(read: str):
