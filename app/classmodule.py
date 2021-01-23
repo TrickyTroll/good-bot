22518e8dd4e9f816cff9a5c65e5d238f7a6a989c
TrickyTroll <tricky@beon.ca>
2021-01-23 15:55:43 -0500

    ✨ New: fake_start to Command object.

--- app/classmodule.py
+++ app/classmodule.py
@@ -1,10 +1,36 @@
 import pexpect
 class Command():
-    def __init__(command: str, arguments: list, read: str):
+    def __init__(command: str, arguments: list):
         self.command = command
         self.arguments = arguments
-        self.read = read
 
+    def fake_start(self, text: str) -> None:
+        """To print the first command before creating a child process.
+
+        Args:
+            text (str): The command that will be used to spawn a child
+            process with pexpect.
+
+        Returns:
+            None: None
+        """
+
+        letters = list(text)
+        for letter in letters[0: -1]:
+            print(letter, end = "", flush = True)
+            time.sleep(.11) #TODO: This should be randomized.
+        print(splitted[-1])
+
+        return None
+
+    def 
+
+    def run(self):
+        pass
+
+class Read():
+    def __init__(read: str):
+        self.to_read = read
 class Edit():
     pass
 
