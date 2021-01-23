02eb15c2f9a8062e77385dd12010749ac7badc4b
TrickyTroll <tricky@beon.ca>
2021-01-23 15:35:24 -0500

    🚧 In progress: the Command class

--- app/classmodule.py
+++ app/classmodule.py
@@ -1,2 +1,12 @@
-class toto():
+import pexpect
+class Command():
+    def __init__(command: str, arguments: list, read: str):
+        self.command = command
+        self.arguments = arguments
+        self.read = read
+
+class Edit():
+    pass
+
+class Slide():
     pass
