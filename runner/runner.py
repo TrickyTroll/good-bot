4c3be84e446513af22c7d7c53cb21aba916c8643
TrickyTroll <tricky@beon.ca>
2021-02-06 20:15:01 -0500

    🚧 In progress: working on the runner

--- runner/runner.py
+++ runner/runner.py
@@ -1,2 +1,9 @@
+import sys
 import classmodule
+import funcmodule
 
+if len(sys.argv()) < 2:
+    print("Please provide a path towards the instructions file")
+    sys.exit()
+
+# TODO: Parse the config (in the funcmodule)
