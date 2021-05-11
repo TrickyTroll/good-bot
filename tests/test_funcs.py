17b7586853c3b7d33941c14c8288e3cc89905f8e
Etienne Parent <etienne.parent@beon.ca>
2021-05-11 11:11:23 -0400

    Tests: now testing `config_info()`

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -48,3 +48,48 @@ class TestParser(unittest.TestCase):
         for item in self.PARSED.values():
             for thing in item:
                 self.assertEqual(type(thing), dict)
+
+class TestInfo(unittest.TestCase):
+    """Tests for the `config_info` function.
+
+    From the function `config_info()`:
+    ----------------------------------
+
+    Testing:
+        * Returns a `dict`.
+        * Keys in the `dict` are of type `int`.
+        * Values in the returned `dict` are of type `dict`.
+
+    From values in the `dict` returned by `config_info()`:
+    ======================================================
+
+    Testing:
+        * Keys are of type `str`.
+        * Keys are contained in ["commands", "expect", "scenes", "editor", "slides", "read"].
+        * Values are of type `list`.
+
+    """
+    parsed = funcmodule.config_parser(CONFIGPATH / "test_conf.yaml")
+
+    RETURNED = funcmodule.config_info(parsed)
+    VALUES = RETURNED.values()
+
+    def test_returns_dict(self):
+        """Testing that `config_parser()` returns a `dict`."""
+        self.assertEqual(type(self.RETURNED), dict)
+    def test_dict_keys(self):
+        """Testing the type of the dictionnary keys.
+
+        They should be of type `int`.
+
+        """
+        for key in self.RETURNED.keys():
+            self.assertEqual(type(key), int)
+    def test_dict_values(self):
+        """Testing the type of the dictionnary values.
+
+        They should be of type `dict`.
+
+        """
+        for value in self.RETURNED.values():
+            self.assertEqual(type(value), dict)
