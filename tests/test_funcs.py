8449f147186fe7fbc0d5665ea293273bf51c3e9e
TrickyTroll <53831454+TrickyTroll@users.noreply.github.com>
2021-06-06 15:40:12 -0400

    Tests: testing `is_scene()`

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -254,3 +254,22 @@ def test_split_config_read():
             continue
 
     assert read_amount == read_expected
+
+def test_is_scene_false():
+    """
+    Makes sure that the `is_scene()` function does not recognize
+    a project path as a scene path.
+    """
+    not_a_scene_path = PROJECT_PATH
+    assert funcmodule.is_scene(not_a_scene_path) == False
+
+def test_is_scene_true():
+    """
+    Makes sure that the `is_scene()` function recognizes proper
+    scene paths.
+    """
+    a_scene_path = PROJECT_PATH / "scene_1"
+    assert funcmodule.is_scene(a_scene_path)
+
+def test_list_scenes():
+    pass
