1ed4ad93093f2a9f7b85f9b05be6e1be1cab1648
TrickyTroll <53831454+TrickyTroll@users.noreply.github.com>
2021-06-22 16:29:45 -0400

    New: `render` module

---
+++ goodbot/render.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""`goodbot`'s render module.
+
+Contains functions used by the good-bot-cli app to render asciicast
+recordings in a mp4 video.
+
+Uses prerendered gifs and audio from Google TTS to make the final
+rendering.
+
+The conversion asciicast -> gif is done using the asciicast2gif
+docker image.
+
+This module requires ffmpeg.
+"""
