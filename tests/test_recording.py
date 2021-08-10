d0af5a77a5385213902c85b14a612cc25dc971c3
Etienne Parent <53831454+TrickyTroll@users.noreply.github.com>
2021-08-10 15:54:53 -0400

    Tests: testing new runner fetchers

--- tests/test_recording.py
+++ tests/test_recording.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 from goodbot import recording
 from tests.test_funcs import PROJECT_PATH, PARSED
+
+VIDEO_TEST_DIR = Path("./tests/examples/video")
 
 def test_is_scene_false():
     """
@@ -36,3 +39,65 @@ def test_list_scenes():
     assert len(all_scenes) == len(listed_scenes) and sorted(
         all_scenes
     ) == sorted(listed_scenes)
+
+
+def test_fetch_runner_instructions():
+    """
+    Making sure that fetch_runner_instructions finds enough instructions
+    files
+    """
+    to_check = [
+        {
+            "file": VIDEO_TEST_DIR / Path("scene_1/commands"),
+            "want": 2
+        },
+        {
+            "file": VIDEO_TEST_DIR / Path("scene_2/commands"),
+            "want": 1
+        },
+        {
+            "file": VIDEO_TEST_DIR / Path("scene_3/commands"),
+            "want": 1
+        },
+        {
+            "file": VIDEO_TEST_DIR / Path("scene_fake/commands"),
+            "want": 0
+        }
+    ]
+    for test_case in to_check:
+        assert len(recording.fetch_runner_instructions(test_case["file"])) == test_case["want"]
+
+def test_fetch_scene_runner_instructions():
+    """
+    Testing that fetch_scene_runner_instructions returns the correct
+    amount of items.
+    """
+    to_check = [
+        {
+            "dir": VIDEO_TEST_DIR / Path("scene_1"),
+            "want": 2
+        },
+        {
+            "dir": VIDEO_TEST_DIR / Path("scene_2"),
+            "want": 1
+        },
+        {
+            "dir": VIDEO_TEST_DIR / Path("scene_3"),
+            "want": 1
+        },
+        {
+            "dir": VIDEO_TEST_DIR / Path("scene_fake"),
+            "want": 0
+        }
+    ]
+
+    for test_case in to_check:
+        assert len(recording.fetch_scene_runner_instructions(test_case["dir"])) == test_case["want"]
+
+def test_fetch_project_runner_instructions():
+    """
+    Testing that fetch_project_runner_instructions finds the right
+    amount of files.
+    """
+    want = 4
+    assert len(recording.fetch_project_runner_instructions(VIDEO_TEST_DIR)) == want
