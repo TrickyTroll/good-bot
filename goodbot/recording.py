4ec1c1cd423dec9296b2d62eb3b3e384f58e2f03
Etienne Parent <tricky@pop-os.localdomain>
2021-10-07 12:37:35 -0400

    Change: moving functions to utils.py

--- goodbot/recording.py
+++ goodbot/recording.py
@@ -10,70 +10,7 @@ from rich.console import Console
 from pathlib import Path
 from typing import List, Dict, Union, Any
 
-
-def is_scene(directory: Path) -> bool:
-    """Checks if a directory is a scene that contains instructions.
-
-    To be a scene, a directory must:
-
-        * Contain files.
-        * Its name must start with `scene`.
-
-    Args
-        directory (Path): The path towards the directory to
-        check.
-    Returns:
-        bool: Whether the directory is a scene that contains elements
-        or not.
-    """
-
-    if directory.is_dir():
-        dir_name = directory.name
-        contains_something = any(directory.iterdir())
-    else:
-        return False
-
-    return dir_name[0:5] == "scene" and contains_something
-
-
-def list_scenes(project_dir: Path) -> List[Path]:
-    """Lists every scene contained in the `project_dir` path.
-
-    `list_scenes` uses the `is_scene` function to check whether
-    or not a directory si a "scene".
-
-    To be a scene, a directory must:
-
-        * Contain files.
-        * Its name must start with `scene`.
-
-    This function will also tell the user if a subdirectory of
-    `project_dir` was ignored.
-
-    Args:
-        project_dir (Path): The path towards the directory that
-            potentially contains scenes.
-
-    Returns:
-        List[Path]: A `list` of `Path`s towards each scene contained
-            in `project_dir`. If `project_dir` did not contain
-            any scene, the returned `list` will be empty.
-
-    """
-
-    all_scenes: List[Path] = []
-
-    for directory in project_dir.iterdir():
-
-        if is_scene(directory):
-
-            all_scenes.append(directory)
-
-        else:
-
-            print(f"The directory {directory} was ignored.")
-
-    return all_scenes
+from goodbot import utils
 
 
 def is_runner_instructions(instructions_path: Path) -> bool:
@@ -92,7 +29,7 @@ def is_runner_instructions(instructions_path: Path) -> bool:
         bool: Whether or not the file is an instructions file for Good
         Bot Runner.
     """
-    if instructions_path.suffix == ".yaml" or instructions_path.suffix == "":
+    if instructions_path.suffix in utils.ALLOWED_INSTRUCTION_SUFFIX:
         with open(instructions_path, "r") as stream:
             contents: str = stream.read()
             try:
@@ -185,7 +122,6 @@ def fetch_project_runner_instructions(project_path: Union[Path, str]) -> List[Pa
                 scene
             )
     return all_runner_instructions
-
 
 def record_commands(project: Path, debug: bool = False) -> List[Path]:
     """Records a gif for every video in the commands directory of the
--- tests/test_recording.py
+++ tests/test_recording.py
@@ -9,41 +9,6 @@ from tests.test_funcs import PROJECT_PATH, PARSED
 VIDEO_TEST_DIR = Path("./tests/examples/video")
 
 
-def test_is_scene_false():
-    """
-    Makes sure that the `is_scene()` function does not recognize
-    a project path as a scene path.
-    """
-    not_a_scene_path = PROJECT_PATH
-    assert recording.is_scene(not_a_scene_path) == False
-
-
-def test_is_scene_true():
-    """
-    Makes sure that the `is_scene()` function recognizes proper
-    scene paths.
-    """
-    a_scene_path = PROJECT_PATH / "scene_1"
-    assert recording.is_scene(a_scene_path)
-
-
-def test_list_scenes():
-    """Testing that `list_scenes()` really lists every scene.
-
-    This test assumes that the previous tests passed. If they
-    didn't, this might fail even if `list_scenes()` is right.
-
-    For example, if some directories are not created by the
-    `create_dirs()` function, the amount of scenes in the file
-    and the amount of scene directories won't match.
-
-    """
-    scene_amount = len(PARSED.keys())
-    listed_scenes = recording.list_scenes(PROJECT_PATH)
-    all_scenes = [PROJECT_PATH / f"scene_{i+1}" for i in range(scene_amount)]
-    assert len(all_scenes) == len(listed_scenes) and sorted(all_scenes) == sorted(listed_scenes)
-
-
 def test_fetch_runner_instructions():
     """
     Making sure that fetch_runner_instructions finds enough instructions
