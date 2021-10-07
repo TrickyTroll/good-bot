16e3bf58b40966e506290ba14456eaa77f454313
Etienne Parent <tricky@pop-os.localdomain>
2021-10-07 12:38:03 -0400

    New: utils file for utility functions
    For functions that are used in more than one module

---
+++ goodbot/utils.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+utils.py
+"""
+
+ALLOWED_INSTRUCTIONS_SUFFIX = (".yaml", ".txt", "")
+
+def is_scene(directory: Path) -> bool:
+    """Checks if a directory is a scene that contains instructions.
+
+    To be a scene, a directory must:
+
+        * Contain files.
+        * Its name must start with `scene`.
+
+    Args
+        directory (Path): The path towards the directory to
+        check.
+    Returns:
+        bool: Whether the directory is a scene that contains elements
+        or not.
+    """
+
+    if directory.is_dir():
+        dir_name = directory.name
+        contains_something = any(directory.iterdir())
+    else:
+        return False
+
+    return dir_name[0:5] == "scene" and contains_something
+
+
+def list_scenes(project_dir: Path) -> List[Path]:
+    """Lists every scene contained in the `project_dir` path.
+
+    `list_scenes` uses the `is_scene` function to check whether
+    or not a directory si a "scene".
+
+    To be a scene, a directory must:
+
+        * Contain files.
+        * Its name must start with `scene`.
+
+    This function will also tell the user if a subdirectory of
+    `project_dir` was ignored.
+
+    Args:
+        project_dir (Path): The path towards the directory that
+            potentially contains scenes.
+
+    Returns:
+        List[Path]: A `list` of `Path`s towards each scene contained
+            in `project_dir`. If `project_dir` did not contain
+            any scene, the returned `list` will be empty.
+
+    """
+
+    all_scenes: List[Path] = []
+
+    for directory in project_dir.iterdir():
+
+        if is_scene(directory):
+
+            all_scenes.append(directory)
+
+        else:
+
+            print(f"The directory {directory} was ignored.")
+
+    return all_scenes
