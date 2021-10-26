9c4152b73c104d93a24692b8b5088137fe05f0a5
Étienne Parent <53831454+TrickyTroll@users.noreply.github.com>
2021-10-26 02:54:28 +0000

    Todo: added todos for recording implementation

--- goodbot/recording.py
+++ goodbot/recording.py
@@ -1,4 +1,14 @@
+# Each recording module has to be imported here
+from goodbot import editor, shell_commands, audio
 from goodbot.utils import is_scene
+
+def find_to_record():
+    # TODO: Find each thing to record *in order*
+    pass
+
+def record_scene(scene_path: Path):
+    # TODO: Record each command in a scene in order.
+    pass
 
 def record_project(project_path: Path):
     for potential_scene in project_path.iterdir():
