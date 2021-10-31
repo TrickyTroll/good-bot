4bd4308440e0e3912a6918e9f80cc13e92a15451
Étienne Parent <53831454+TrickyTroll@users.noreply.github.com>
2021-10-31 03:13:20 +0000

    In progress: testing the new recording module

---
+++ tests/test_recording.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from goodbot import recording
+
+def test_get_content_file_id_works_with_strings():
+    """
+    Test that get_content_file_id works with strings
+    """
+    assert recording.get_content_file_id("commands_1.yaml") == 1
+
+def test_get_content_file_id():
+    """
+    Test that `get_content_file_id()` returns the correct
+    value for different paths.
+
+    A file id is the integer value after a `_` character in
+    a file name.
+    """
+    all_samples = [
+        (Path("/foo/bar_1.yaml"), 1),
+        (Path("/foo/bar_2.yaml"), 2),
+        ("/foo/bar_3.yaml", 3),
+        ("../example/commands_0.yaml", 0)
+    ]
+
+    for sample in all_samples:
+        assert recording.get_content_file_id(sample[0]) == sample[1]
