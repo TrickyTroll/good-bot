79da4157ab6a6152a2de0bd161cd2ee559146654
Etienne Parent <53831454+TrickyTroll@users.noreply.github.com>
2021-11-20 22:06:39 -0500

    Tests: test for the new `sort_scenes`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -39,3 +39,23 @@ def test_list_scenes():
     listed_scenes = utils.list_scenes(PROJECT_PATH)
     all_scenes = [PROJECT_PATH / f"scene_{i+1}" for i in range(scene_amount)]
     assert len(all_scenes) == len(listed_scenes) and sorted(all_scenes) == sorted(listed_scenes)
+
+def test_sort_scenes():
+    """Making sure that `sort_scnees()` sorts the scenes properly.
+    """
+    test_cases = (
+        [Path("scene_1"), Path("scene_2"), Path("scene_3")],
+        [Path("scene_3"), Path("scene_2"), Path("scene_1")],
+        [Path("scene_2")],
+        []
+    )
+
+    want = (
+        [Path("scene_1"), Path("scene_2"), Path("scene_3")],
+        [Path("scene_1"), Path("scene_2"), Path("scene_3")],
+        [Path("scene_2")],
+        []
+    )
+
+    for index, test in enumerate(test_cases):
+        assert utils.sort_scenes(test) == want[index]
